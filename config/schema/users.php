30253e52c0e6f5b7ed053274c8601ac22b6704ca
James Larking <jim@citadelsecure.com>
2010-11-26 07:20:49 +0000

    continued personal configuration

--- config/schema/users.php
+++ config/schema/users.php
@@ -43,13 +43,6 @@ class usersSchema extends CakeSchema {
 		'role' => array('type' => 'string', 'null' => true, 'default' => NULL),
 		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
 		'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
-		'group_id'=>array('type' => 'integer', 'null' => true, 'default' => NULL),
 		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1), 'BY_USERNAME' => array('column' => array('username', 'passwd'), 'unique' => 0), 'BY_EMAIL' => array('column' => array('email', 'passwd'), 'unique' => 0))
 	);
-	
-	var $groups = array(
-		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
-		'name' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 250),
-		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1))
-	);	
 }
---
+++ controllers/components/simple_auth.php
@@ -0,0 +1,31 @@
+<?php
+class SimpleAuthComponent extends Object{
+	var $components=array("Auth", "Session");
+	
+	function initialize($controller){
+		/*
+		* Auth Config
+		*/
+        $controller->Auth->fields = array('username' => 'email','password' => 'passwd');
+        $controller->Auth->loginAction = array('plugin'=>"users", 'controller' => 'users', 'action' => 'login', "admin"=>false);
+        $controller->Auth->logoutRedirect = array('plugin'=>"users", 'controller' => 'users', 'action' => 'login', "admin"=>false);
+        $controller->Auth->loginRedirect = array('plugin'=>'users', 'controller' => 'users', 'action' => 'dashboard', "admin"=>false);        
+	}
+	function startup($controller){
+		/*
+		* Simple Prefix Based Auth
+		*/
+		if(!empty($controller->params['prefix']) &&  $controller->Session->read("Auth.User.role")!=$controller->params['prefix'] ){
+			$controller->Session->setFlash("You are not allowed to access that location");
+			if($controller->Session->check("Auth")){
+				$controller->redirect("/");
+			}else{
+				$controller->redirect($controller->Auth->loginAction);
+			}
+			exit();
+		}else{
+			$controller->Auth->allow($controller->action);
+		}	
+	}
+}
+?>
